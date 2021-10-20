eb556048eaa5ab208b6cb1516ea7bdf39e7dafc2
iburangulov <iburanguloff@ya.ru>
2021-10-21 00:32:38 +0500

    init

---
+++ core/Application.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Core;
+
+final class Application
+{
+
+    public function init(): void
+    {
+
+    }
+
+    public function start(): void
+    {
+
+    }
+
+    public function finish(): void
+    {
+
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
-<?php
+<?php declare(strict_types=1);
 
-phpinfo();
+use core\Application;
+
+define('ROOT_PATH', dirname(__DIR__));
+
+require_once ROOT_PATH . '/vendor/autoload.php';
+
+$application = new Application();
+
+$application->init();
+$application->start();
+$application->finish();
