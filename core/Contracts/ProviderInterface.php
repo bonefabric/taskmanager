782b6db0a09e78abcb1f7c0566f9d85ab98e84e7
iburangulov <iburanguloff@ya.ru>
2021-10-22 00:00:07 +0500

    services & providers

--- core/Contracts/ProviderInterface.php
+++ core/Contracts/ProviderInterface.php
@@ -5,4 +5,11 @@ namespace Core\Contracts;
 interface ProviderInterface
 {
 
+	public function boot(): void;
+
+	/**
+	 * @param ServiceInterface $service
+	 */
+	public function register(ServiceInterface $service): void;
+
 }
--- core/Contracts/ServiceInterface.php
+++ core/Contracts/ServiceInterface.php
@@ -5,4 +5,6 @@ namespace Core\Contracts;
 interface ServiceInterface
 {
 
+
+
 }
