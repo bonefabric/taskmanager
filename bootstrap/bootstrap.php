6d64b54228ef9fc2e661f514d2147e07256faeb7
iburangulov <iburanguloff@ya.ru>
2021-10-22 11:07:12 +0500

    exception handler

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+use Core\Components\Utils\ExceptionHandler;
 use Dotenv\Dotenv;
 
+ExceptionHandler::init();
 (Dotenv::createImmutable(ROOT_PATH))->safeLoad();
---
+++ core/Components/Utils/ExceptionHandler.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Core\Components\Utils;
+
+use Core\Components\Helpers\Template;
+use Symfony\Component\HttpFoundation\Response;
+
+class ExceptionHandler
+{
+
+	public static function init(): void
+	{
+		set_exception_handler(static function (\Exception $exception) {
+			if ($_ENV['DEBUG'] !== 'true') {
+				(new Response(Template::getTemplate('errors.500')))->send();
+				return;
+			}
+			dump($exception);
+		});
+	}
+
+}
