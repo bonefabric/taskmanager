0f5905d98ae73a70bfc4383ee7e78c99c0824452
iburangulov <iburanguloff@ya.ru>
2021-10-25 06:43:43 +0500

    ui button

---
+++ resources/js/components/ui/uiButton.vue
@@ -0,0 +1,33 @@
+<template>
+	<button class="p-1 pl-5 pr-5 border-2 border-blue-900 bg-blue-200 hover:bg-blue-800 hover:text-white w-32" @click.stop="clicked"><slot></slot></button>
+</template>
+
+<script>
+/** Button reloading */
+const RELOADING = 700;
+
+export default {
+	name: "uiButton",
+	data() {
+		return {
+			disabled: false,
+		}
+	},
+	methods: {
+		clicked: function () {
+			if (this.disabled) {
+				return;
+			}
+			setTimeout(() => {
+				this.disabled = false;
+			}, RELOADING);
+			this.$emit('click');
+			this.disabled = true;
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
